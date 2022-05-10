f08ff775c1c00cda40f9a75484254062f4cd1010
NOE VAZQUEZ P <famvazpom@gmail.com>
2022-05-09 21:28:55 -0500

    Cambios al blog Se agrego landing funcional

--- blog/views.py
+++ blog/views.py
@@ -8,6 +8,13 @@ from .models import *
 from .forms import *
 
 
+#Error 403 - Forbidden
+def handler404(request,exception):
+    return render(request, 'blog/page-404.html',status=404)
+
+def handler403(request,exception):
+    return render(request, 'blog/page-403.html',status=403)
+
 def login_view(request):
     form = LoginForm(request.POST or None)
 
